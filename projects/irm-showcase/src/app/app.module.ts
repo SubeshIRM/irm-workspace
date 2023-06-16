22e833d33b431e3733bc88c4afbeb04154863638
SubeshIRM <subesh.yadav@irmcloud.info>
2023-06-16 11:37:08 +0530

    Added the zipped file of release

--- projects/irm-showcase/src/app/app.module.ts
+++ projects/irm-showcase/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { IrmDffModule } from '../../../irm-dff/src/public-api';
+// import { IrmDffModule } from '../../../irm-dff/src/public-api';
+import { IrmDffModule } from 'irm-dff';
 import { AppComponent } from './app.component';
 
 @NgModule({
